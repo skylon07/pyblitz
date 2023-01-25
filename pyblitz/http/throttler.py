5d123095d1fdbdcfa418b39c0fb57e13fea6584c
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2023-01-25 13:47:16 -0700

    Fix request sendRequest() not returning response

--- pyblitz/http/throttler.py
+++ pyblitz/http/throttler.py
@@ -1,12 +1,13 @@
 from threading import Timer
 from typing import Union
+from time import sleep
 
 from .requests import Request
+
 
 class RequestThrottler:
     def __init__(self):
         self._throttle = 0
-        self._requestQueue = []
         self._pendingTimer = None
 
     def setThrottle(self, requestRateSecs: Union[float, int]):
@@ -14,25 +15,32 @@ class RequestThrottler:
         self._throttle = requestRateSecs
 
     def sendRequest(self, request: Request):
-        self._requestQueue.append(request)
-        self._attemptSendNextRequest()
+        self._waitUntilOutOfTimeout()
+        self._startTimeout()
+        return request.send()
 
     @property
-    def _requestIsPending(self):
+    def _inTimeout(self):
         return self._pendingTimer is not None
 
-    def _attemptSendNextRequest(self):
-        if len(self._requestQueue) == 0 or self._requestIsPending:
-            return
-        assert self._pendingTimer is None
+    def _waitUntilOutOfTimeout(self):
+        sleepTime = self._throttle / 100
+        sleepsDone = 0
+        while self._inTimeout:
+            sleep(sleepTime)
+            sleepsDone += 1
+            if sleepsDone == 50:
+                sleepTime *= 10
+            elif sleepsDone == 105:
+                sleepTime *= 5
 
+    def _startTimeout(self):
         if self._throttle > 0:
             self._pendingTimer = Timer(self._throttle, self._onTimerEnd)
             self._pendingTimer.start()
-        nextRequest = self._requestQueue.pop(0)
-        nextRequest.send()
 
     def _onTimerEnd(self):
+        self._clearTimer()
+
+    def _clearTimer(self):
         self._pendingTimer = None
-        self._attemptSendNextRequest()
-    
